682c24544e928506d8d9105fe0d693a9bd29d889
renfeng <renfeng@orieange.com>
2019-01-21 21:03:45 +0800

    第二次

--- js/public.js
+++ js/public.js
@@ -1,9 +1,9 @@
-$('#case_anchor').hover(
+$('#case_anchor').hover( //导航栏选符
     function(){
-        $('#case_down').slideDown()
+        $('#case_down').slideDown(200)
     },
     function(){
-        $('#case_down').slideUp()
+        $('#case_down').slideUp(200)
     }
 )
 $('#case_down').hover(
@@ -14,3 +14,6 @@ $('#case_down').hover(
         $('#case_down').slideUp()
     }
 )
+$('.prise').click(function(){ //点赞
+	$(this).toggleClass('on')
+})
